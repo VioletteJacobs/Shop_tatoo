5bf81e0e48d6785bc696c5917cf5084792664b79
Violette Jacobs <72454626+VioletteJacobs@users.noreply.github.com>
2021-05-27 19:48:47 +0200

    configuration Heroku

---
+++ resources/views/template/main.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="Fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Shop tatoo</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Shop Tatoo</h1>
+    </div>
+</body>
+</html>
