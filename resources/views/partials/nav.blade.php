2bc1b22e6268d965d12a4674b91ca7c2af91a2a9
Violette Jacobs <72454626+VioletteJacobs@users.noreply.github.com>
2021-06-01 17:18:53 +0200

    test

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,10 +1,10 @@
-<header class="d-flex">
+<header class="d-flex ">
     <ul >
-        <li>Portfolio</li>
+        <li class="text-danger">Portfolio</li>
         <li>A propos</li>
         <li>Me contacter</li>
     </ul>
     <div class="banner">
-        
+        <h1>Hello</h1>
     </div>
 </header>
--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Shop tatoo</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="{{secure_asset("style.css")}}">
 </head>
 <body>
     @include('partials.nav')
