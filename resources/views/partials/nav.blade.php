c4e2d4157be2f78b1b067c25d76d72419160c983
Violette Jacobs <72454626+VioletteJacobs@users.noreply.github.com>
2021-06-03 17:42:59 +0200

    style header

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,8 +1,11 @@
 <header>
     <div id="banner">
         <h1 id="titre" class="horizontal">L'encrier moderne</h1>
+        <h2 id="titre2">Tattoo Shop</h2>
         <h3 id="since" class="horizontal">Depuis 1987</h3>
-        <button id="rdv">Prendre rendez-vous ?</button>
+        <div>
+            <button id="rdv">Prendre rendez-vous ?</button>
+
+        </div>
     </div>
-    <div class="distortion"></div>
 </header>
--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Shop tatoo</title>
-    <link rel="stylesheet" href="{{secure_asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('/css/app.css')}}">
 </head>
 <body>
     @include('partials.nav')
